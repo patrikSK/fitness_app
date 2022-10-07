3519ddeb56a6a77d69ea4325da0ca5e5b1e4926b
patrik <husnajpatrik@gmail.com>
2022-10-07 10:22:52 +0200

    create getUniqueDates fn

--- src/helpers/dateHandlers.js
+++ src/helpers/dateHandlers.js
@@ -3,7 +3,6 @@
  * this fn format date into new desired form
  * @returns {string} - date in human readable format: '29 08 2022'
  */
-
 const dateWithMontName = (date = "") => {
   const monthAndYear = new Date(date).toLocaleString("en-us", {
     month: "long",
@@ -14,9 +13,31 @@ const dateWithMontName = (date = "") => {
   return formatedDate;
 };
 
+/**
+ *
+ * @param {string} date
+ * this fn get raw date and return day and month
+ * @returns {string}
+ */
 const dayAndMonth = (date = "") => {
   let splitedDate = date.split("-");
   return `${splitedDate[2]}. ${splitedDate[1]}.`;
 };
 
-export { dateWithMontName, dayAndMonth };
+/**
+ *
+ * @param {[]} records
+ * return array of unique dates from allrecords
+ * @returns {string[]}
+ */
+const getUniqueDates = (records = []) => {
+  let dates = [];
+  records.forEach((record) => {
+    if (!dates.includes(record.date)) {
+      dates.push(record.date);
+    }
+  });
+  return dates;
+};
+
+export { dateWithMontName, dayAndMonth, getUniqueDates };
